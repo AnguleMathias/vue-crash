436b8c36e61e860eb838a0061ca7f75a6d1e834a
Angule Mathias <angulemathias3@gmail.com>
2023-09-14 10:10:19 +0300

    header body and footer components

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>My Application</h1>
+  <header-component />
+  <body-component />
+  <footer-component />
 </template>
 
 <script>
-export default {};
+import HeaderComponent from './components/HeaderComponent.vue';
+import BodyComponent from './components/BodyComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'body-component': BodyComponent,
+    'footer-component': FooterComponent,
+    'header-component': HeaderComponent
+  }
+};
 </script>
 
 <style>
