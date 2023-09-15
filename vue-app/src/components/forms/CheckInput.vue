64b2aaf5562d63d603655b17e06c03dcc7445f8a
Angule Mathias <angulemathias3@gmail.com>
2023-09-15 07:06:53 +0300

    form component

---
+++ vue-app/src/components/forms/CheckInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mb-3 form-check">
+    <input
+      :id="name"
+      :name="name"
+      :checked="checked"
+      type="checkbox"
+      class="form-check-input"
+      :required="required"
+      :value="value"
+    />
+    <label :for="name" class="form-check-label">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckInput",
+  props: ["name", "label", "required", "value", "checked"],
+};
+</script>
